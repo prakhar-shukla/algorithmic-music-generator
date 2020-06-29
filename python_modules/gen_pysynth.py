eda9e5e3cbee4cb8f0cfb6501bf06e65433b0e16
prakhar-shukla <prakharshuklaofficial@gmail.com>
2020-06-29 23:45:43 +0530

    Updated tracking info

--- python_modules/gen_pysynth.py
+++
@@ -1,8 +0,0 @@
-from constants import *
-
-def toPsNote(chromosone):
-    # assert isinstance(note, basestring)
-    # assert len(node) == 1
-    # assert note in NOTES
-    return (note, DURATION)
-
